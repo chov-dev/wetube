1c88fd4c87908274fd3c8f9025d6d63b026e4347
chov-dev <56998563+chov-dev@users.noreply.github.com>
2021-03-27 20:44:16 +0900

    setup nodemon

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
 import express from 'express';
 const app = express();
 
-const handleListening = () => {
-  console.log('✅ Listening on: http://localhost:5000');
-};
+const handleListening = () => console.log('✅ Listening on: http://localhost:5000');
+const handleHome = (req, res) => res.send('Hello from home');
 
-
-app.get('/', (req, res) => {
-  res.send('Hello world!');
-});
-
+app.get('/', handleHome);
 
 app.listen(5000, handleListening);
